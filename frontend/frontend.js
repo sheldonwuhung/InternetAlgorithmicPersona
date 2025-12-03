1748b83b3d80dfe8690e806ee42312eba7007b1a
hedgehogz <sheldonwhung@gmail.com>
2025-12-03 03:06:57 -0500

    backend test 14

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -49,7 +49,7 @@ async function send() {
       if (!response.ok) console.log("oh no");
       console.log(data.result); 
     } catch (error) {
-      console.log("error")
+      console.log(error);
     }
 }
 
