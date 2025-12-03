5393828efcbca689ff43fd2fac8c14038ae54576
hedgehogz <sheldonwhung@gmail.com>
2025-12-03 17:13:20 -0500

    backend test 20

--- api/backend.js
+++ api/backend.js
@@ -6,6 +6,10 @@
 import { GoogleGenAI, createUserContent } from "@google/genai";
 
 export default async function handler(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
     try {
         const { prompt, images } = req.body;
         console.log(images);
