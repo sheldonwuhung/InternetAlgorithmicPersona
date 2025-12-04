fee5ce709aad79657651f1087bcdf31f31d2ad4e
hedgehogz <sheldonwhung@gmail.com>
2025-12-03 22:05:23 -0500

    backend test 24-3

--- api/backend.js
+++ api/backend.js
@@ -9,7 +9,6 @@ export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
     try {
         const { prompt, images } = req.body;
         console.log(images);
