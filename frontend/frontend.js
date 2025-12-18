556acbc39d595b6f26673a16159e6cc7eb6151ed
hedgehogz <sheldonwhung@gmail.com>
2025-12-18 00:55:31 -0500

    prompt test

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -197,13 +197,10 @@ async function send() {
       dataP.style.transform = "translate(0, 0)";
       dataP.style.paddingBottom = "0";
 
-      let paragraphs = dataP.children;
-      console.log(paragraphs);
+      let paragraphs = dataP.querySelectorAll('p');
 
       paragraphs.forEach(p => {
-        if (p instanceof HTMLParagraphElement) {
-          p.className = "report-section";
-        }
+        p.className = "report-section";
       });
 
   } catch (error) {
